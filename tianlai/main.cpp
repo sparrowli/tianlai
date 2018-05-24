15dc5f5d8a347ad66e046abdf4a333dfe655203f
sparrow <280669537@qq.com>
2018-05-24 18:24:57 +0800

    Initialize project. :pear: sparrowli

---
+++ tianlai/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tianlai
+//
+//  Created by sparrow on 24/5/2018.
+//  Copyright © 2018 sparrow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
